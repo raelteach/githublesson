9591c86b947259aac83033c47244815239a2fe1d
Rael Liss <rael.lissoos@iacd.io>
2023-06-13 10:16:59 +0200

    we added a multiplication function

--- gittest.playground/Contents.swift
+++ gittest.playground/Contents.swift
@@ -12,4 +12,9 @@ func sum(_ num1: Int, _ num2: Int) -> Int {
 print(sum(20, 30))
 print(sum(20, 40))
 
+func mult(_ num1: Int, _ num2: Int) -> Int {
+    
+    return num1 * num2
+}
 
+print(mult(10, 20))
